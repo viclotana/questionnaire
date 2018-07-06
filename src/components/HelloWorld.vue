ca92de7a990284dd82186dbbbc6033fb4efba2c2
vic lotana <viclotana@gmail.com>
2018-07-06 17:43:49 +0100

    data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,23 @@
 <script>
-
+let gifs = [
+  "https://media.giphy.com/media/l4pSVziIbNt3WmJ8c/giphy.gif",
+  "https://media.giphy.com/media/1zSz5MVw4zKg0/giphy.gif",
+  "https://media.giphy.com/media/5xtDarC0XyqmUhD5eDK/giphy.gif",
+  "https://media.giphy.com/media/12ETcAyWE6TjHy/giphy.gif",
+  "https://media.giphy.com/media/T5QOxf0IRjzYQ/giphy.gif",
+  "https://media.giphy.com/media/YWFmlljmSpo6k/giphy.gif",
+  "https://media.giphy.com/media/tCYMesnacJ6cE/giphy.gif",
+  "https://media.giphy.com/media/67T9Wfqpd9z6E8nvdx/giphy.gif",
+  "https://media.giphy.com/media/12ETcAyWE6TjHy/giphy.gif",
+  "https://media.giphy.com/media/oxFDq4E9CHb7W/giphy.gif",
+  "https://media.giphy.com/media/5xtDarDQqitychRz27K/giphy.gif",
+  "https://media.giphy.com/media/tpdG5dt17HaO4/giphy.gif",
+  "https://media.giphy.com/media/10Jpr9KSaXLchW/giphy.gif",
+  "https://media.giphy.com/media/PS7d4tm1Hq6Sk/giphy.gif",
+  "https://media.giphy.com/media/I4hvxPEWSbq5q/giphy.gif",
+  "https://media.giphy.com/media/wofftnAdDtx4s/giphy.gif"
+]
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  }
+  name: 'App',
 }
 </script>
