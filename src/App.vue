8558d7a0a4033d40cf02e5a9ea66ae28209e9327
vic lotana <viclotana@gmail.com>
2018-07-06 17:09:17 +0100

    second

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,9 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <form>
+      <input placeholder="What is your question?" />
+    </form>
+    <div class="gif-container">
+    </div>
   </div>
 </template>
-
-<script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
